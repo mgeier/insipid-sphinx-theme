d8a7ba20c905939b515a7d823f5f484b9a080bf2
Matthias Geier <Matthias.Geier@gmail.com>
2020-08-04 19:13:26 +0200

    Enable readthedocs-sphinx-search
    See https://github.com/readthedocs/readthedocs-sphinx-search

--- doc/conf.py
+++ doc/conf.py
@@ -132,6 +132,7 @@ html_secnumber_suffix = '\N{FIGURE SPACE}'
 # -- Sphinx extensions --------------------------------------------------------
 
 extensions = [
+    'sphinx_search.extension',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
