48b10981ca45a4f2b789986d77ca640c1c2e7850
Matthias Geier <Matthias.Geier@gmail.com>
2022-06-04 16:57:44 +0200

    Add more branches to git-rebase script

--- git_rebase_rtd_branches.sh
+++ git_rebase_rtd_branches.sh
@@ -10,6 +10,8 @@ git push origin HEAD:%(refname:lstrip=3) --force
 
 eval "$(git for-each-ref --shell --format="$fmt" \
     refs/remotes/origin/breadcrumbs \
+    refs/remotes/origin/sphinx3 \
+    refs/remotes/origin/sphinx4 \
     refs/remotes/origin/readthedocs-sphinx-search \
     refs/remotes/origin/rightsidebar \
     )"
